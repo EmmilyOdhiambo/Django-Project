ab4d81913ebc67a2778f9386965c7429377ddce5
EmmilyOdhiambo <emmilysodhiambo@gmail.com>
2023-08-11 21:46:04 +0300

    Green Kiosk Project

--- Vendor/admin.py
+++ Vendor/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+
 # Register your models here.
+
+from.models import Vendor
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ("email", "password", "first_name", "last_name", "phone_number", "date_of_birth")
+admin.site.register(Vendor,VendorAdmin)
+
+
+
--- inventorry/urls.py
+++ inventorry/urls.py
@@ -1,7 +1,23 @@
+# from django.urls import path
+# from .views import product_upload_view, products_list, product_details
+
+# urlpatterns = [
+#     path("products/upload/",product_upload_view),
+#     path("products/list", products_list, name = "products_list_view"),
+#     path ("products/<int:id>/", product_details, name="product_detail")
+# ]
 from django.urls import path
-from .views import product_upload_view
+from inventorry.views import product_upload, product_list, product_details
+# from .views import product_upload,product_list,product_details
+from .import views
+
+
+
 
 urlpatterns = [
-    path("products/upload/",product_upload_view,
-    name="product_upload_views"),
-]
+    # path('products/upload', upload_product, name='product_upload_view'),
+    path("products/upload/",product_upload),
+    path("products/list",product_list,name= "product_list_view"),
+    path("products/<int:id>/", product_details, name="product_detail_view"),
+    # path("products/edit/<int:id>", edit_product, name="product_edit_view"),
+    ]
